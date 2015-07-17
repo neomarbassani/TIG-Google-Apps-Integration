7b499793a1b11e055b8280ba5dc8df5e790a8c89
Neomar <neomar.bassani@gmail.com>
2015-07-17 14:20:11 -0300

    Integrating Google Calendar with Pika Tickler

--- cms-custom/extensions/create_tickler/config.php
+++ cms-custom/extensions/create_tickler/config.php
@@ -1,12 +1,7 @@
 <?php
-  
-  $SMTPConfig['user'] = "";
-  $SMTPConfig['password'] = "";
-  
-  $EmailSubject = "Tickler Notification - %clientLastName%";
-  $CalendarSubject = "%clientLastName% - %subject% (%user%)";
+define("CALENDAR_SUBJECT", "%clientLastName% - %subject% (%user%)");
 
-  $FromConfig['mail'] = "no-reply@pikasoftware.com";
-  $FromConfig['name'] = "Pika Software";
+define("CALENDAR_DESCRIPTION", "%description% 
 
+Case: %caseLink%");  
 ?>
--- cms-custom/extensions/google_drive_connector/index.php
+++ cms-custom/extensions/google_drive_connector/index.php
@@ -1,23 +1,12 @@
 <?php
 require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
-
-
-/* The Google Drive configuration file should define the client_id and 
-	client_secret is this format:
-	
-	<?php
-	define("CLIENT_ID", 'abc123');
-	define("CLIENT_SECRET", 'def456');
-	define("UNIQUE_FOLDER_ID", "ghi789");
-	?>
-*/
-require_once('/var/www/html/api/v1/google_drive_config.php');
+require_once 'google-api-php-client/src/contrib/Google_CalendarService.php';
+require_once 'google_config.php';
 
 $url_array = explode('?', 'https://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 define("URL", $url_array[0]);
 define("TOKENS_PATH", dirname(__FILE__) . '/' . "tokens/");
-
 
 class PikaDrive {
   private $gClient;
@@ -29,18 +18,18 @@ class PikaDrive {
     $this->gClient->setClientId(CLIENT_ID);
     $this->gClient->setClientSecret(CLIENT_SECRET);
     $this->gClient->setRedirectUri(URL);
-    $this->gClient->setScopes(array('https://www.googleapis.com/auth/drive'));
+    $this->gClient->setScopes(array('https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/calendar'));
 
     if($username != null && self::setToken($username)){
       self::authenticate();
     }
   }
 
-  function setToken($username, $token = ''){
+  function setToken($username, $authToken = ''){
     $tokenPath = $this->tokenPath . $username;
 
-    if(isset($token) && !empty($token)){
-      $this->token = $this->gClient->authenticate($token);
+    if(isset($authToken) && !empty($authToken)){
+      $this->token = $this->gClient->authenticate($authToken);
       file_put_contents($tokenPath, $this->token);
     }else{
       if(file_exists($tokenPath)){
@@ -69,6 +58,10 @@ class PikaDrive {
   }
 
   function createFolder($folderName, $parentId = null){
+    if(empty($parentId) && defined('UNIQUE_FOLDER_ID')){
+      $parentId = UNIQUE_FOLDER_ID;
+    }
+
     return self::uploadFile("", $folderName, $parentId);
   }
 
@@ -136,11 +129,38 @@ class PikaDrive {
   function isAuthenticated($username){
     $tokenPath = $this->tokenPath . $username;
 
-	if(file_exists($tokenPath)){
-	    return true;
-	}
-
-    return false;
+  	return file_exists($tokenPath);
   }
 
+  function createEvent($to, $summary, $description, $startDate, $endDate = null){
+    $optParams = array(
+      'sendNotifications' => TRUE
+    );
+
+    $event = new Google_Event(); 
+    $event->setSummary($summary);
+    $event->setDescription($description);
+    
+    $start = new Google_EventDateTime();
+    $start->setDateTime($startDate);
+    $event->setStart($start);
+
+    if($endDate == null){
+      $endDate = $startDate;
+    }
+    $end = new Google_EventDateTime();
+    $end->setDateTime($endDate);
+    $event->setEnd($end);
+
+    $attendees = Array();
+    foreach ($to as $e) {
+      $g_ea = new Google_EventAttendee();
+      $g_ea->setEmail($e);
+      $attendees[] = $g_ea;
+    }
+    $event->setAttendees($attendees);
+
+    $service = new Google_CalendarService($this->gClient);
+    return $service->events->insert('primary', $event, $optParams);
+  }
 }
